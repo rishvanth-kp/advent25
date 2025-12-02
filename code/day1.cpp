dc8ca33373c1746950b201427c2496b0f670b133
rishvanth <rishvanth@gmail.com>
2025-12-02 18:35:47 -0500

    added day1 part 1

--- code/day1.cpp
+++ code/day1.cpp
@@ -1,15 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
 
 int
 main (int argc, char *argv[]) {
   try {
     if (argc != 2) {
-      throw std::runtime_error(" [infile]");
-    } 
+      throw std::runtime_error("day1 [infile]");
+    }
+ 
+
+    // 
+    int dial = 50;
+    size_t zero_count = 0;
+
+    // read the input
+    std::ifstream in(argv[1]);
+    string line;
+    while (getline(in, line)) {
+      // cout << line << endl;
+      // parse the direction and the distance
+      char dir = line[0];
+      int dist;
+      if (dir == 'L') {
+        dist = -1 * std::stoi(line.substr(1));
+      }
+      else if (dir == 'R') {
+        dist = std::stoi(line.substr(1));
+      }
+      // cout << dir << "\t" << dist << endl;
+
+      dial += dist;
+      dial %= 100;
+      // cout << dial << "\t";
+      dial = (dial >= 0) ? dial : 100 + dial;
+ 
+      // cout << dial << endl;
+
+      // cout << endl;
+
+      if (!dial) {
+        ++zero_count;
+      }
+
+    }
+    in.close();
+   
+    cout << zero_count << endl;
   }
   catch (const std::exception &e) {
     cerr << "ERROR: " << e.what() << endl;

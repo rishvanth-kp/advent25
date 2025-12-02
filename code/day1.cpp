d91cb1fc29d5a93fa39bc08475a910efcf6e8179
rishvanth <rishvanth@gmail.com>
2025-12-02 13:46:21 -0500

    initial commit

---
+++ code/day1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int
+main (int argc, char *argv[]) {
+  try {
+    if (argc != 2) {
+      throw std::runtime_error(" [infile]");
+    } 
+  }
+  catch (const std::exception &e) {
+    cerr << "ERROR: " << e.what() << endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
